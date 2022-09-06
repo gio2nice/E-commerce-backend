b761005245a2c8bdac74d9593f5cc966610ed8b3
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-05 17:37:58 -0700

    pass

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      password: '',
       dialectOptions: {
         decimalNumbers: true,
       },
