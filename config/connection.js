47821693953153ac3ea8c2b774d3687315a24b7a
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-05 17:28:33 -0700

    yes

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      user: 'root',
-      password: 'root',
+      password: '',
       dialectOptions: {
         decimalNumbers: true,
       },
